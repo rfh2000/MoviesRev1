30cad7ff628fa179c26b29cf097f28e5717c3e71
Richard <rick_pendlebury@hotmail.com>
2017-11-11 16:07:43 +0000

    Add trailer response for api call and model class

---
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/model/TrailerResponse.java
@@ -0,0 +1,30 @@
+package com.richardpendlebury.android.moviesrev1.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TrailerResponse {
+
+    @SerializedName("id")
+    private int id;
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Trailer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Trailer> results) {
+        this.results = results;
+    }
+
+}
