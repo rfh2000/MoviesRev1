a9bafc59528b53920c0bbf607fd00cadd6427779
Richard <rick_pendlebury@hotmail.com>
2017-11-11 11:40:31 +0000

    Enable loading of movie poster image in adapter

--- app/src/main/java/com/richardpendlebury/android/moviesrev1/MovieRecyclerAdapter.java
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/MovieRecyclerAdapter.java
@@ -5,9 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.richardpendlebury.android.moviesrev1.model.Movie;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -30,7 +31,9 @@ public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdap
     @Override
     public void onBindViewHolder(MovieRecyclerAdapter.ViewHolder holder, int position) {
         final Movie movie = mMovies.get(position);
-        holder.tv1.setText(movie.getTitle());
+        Picasso.with(mContext)
+                .load(buildPosterImageString(movie.getPosterPath()))
+                .into(holder.iv);
     }
 
     @Override
@@ -39,10 +42,16 @@ public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdap
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tv1;
+        ImageView iv;
         ViewHolder(View itemView) {
             super(itemView);
-            tv1 = itemView.findViewById(R.id.textview1);
+            iv = itemView.findViewById(R.id.movie_items_image_view);
         }
+    }
+
+    private String buildPosterImageString(String posterRelativePath){
+        final String BASE_URL = "http://image.tmdb.org/t/p/";
+        final String DEVICE_SIZE = "w185";
+        return BASE_URL + DEVICE_SIZE + posterRelativePath;
     }
 }
