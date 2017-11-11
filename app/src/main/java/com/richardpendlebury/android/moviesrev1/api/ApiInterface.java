c4be711cbc2b5a7be022557c3d23172b3f84cf58
Richard <rick_pendlebury@hotmail.com>
2017-11-11 16:08:54 +0000

    Add trailer and review api interface calls

--- app/src/main/java/com/richardpendlebury/android/moviesrev1/api/ApiInterface.java
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/api/ApiInterface.java
@@ -2,6 +2,8 @@ package com.richardpendlebury.android.moviesrev1.api;
 
 import com.richardpendlebury.android.moviesrev1.model.Movie;
 import com.richardpendlebury.android.moviesrev1.model.MoviesResponse;
+import com.richardpendlebury.android.moviesrev1.model.ReviewResponse;
+import com.richardpendlebury.android.moviesrev1.model.TrailerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -18,5 +20,11 @@ public interface ApiInterface {
 
     @GET("discover/movie")
     Call<MoviesResponse> getMostPopular(@Query("api_key") String apiKey, @Query("sort_by") String sortBy);
+
+    @GET("movie/{id}/reviews")
+    Call<ReviewResponse> getReviews(@Path("id") int id, @Query("api_key") String apiKey);
+
+    @GET("movie/{id}/videos")
+    Call<TrailerResponse> getTrailers(@Path("id") int id, @Query("api_key") String apiKey);
 
 }
