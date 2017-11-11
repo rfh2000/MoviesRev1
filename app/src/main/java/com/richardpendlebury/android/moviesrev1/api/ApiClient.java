aa22debd884a2683507eb553c7de0cc679e12959
Richard <rick_pendlebury@hotmail.com>
2017-11-11 10:41:21 +0000

    Increase visibility of api client class

--- app/src/main/java/com/richardpendlebury/android/moviesrev1/api/ApiClient.java
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/api/ApiClient.java
@@ -3,12 +3,12 @@ package com.richardpendlebury.android.moviesrev1.api;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-class ApiClient {
+public class ApiClient {
 
     private static final String BASE_URL = "http://api.themoviedb.org/3/";
     private static Retrofit retrofit = null;
 
-    static Retrofit getClient() {
+    public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
