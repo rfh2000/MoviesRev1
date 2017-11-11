c059480eb8dae1e575446854c71fff819050af38
Richard <rick_pendlebury@hotmail.com>
2017-11-11 16:07:20 +0000

    Add review response for api call and model class

---
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/model/ReviewResponse.java
@@ -0,0 +1,59 @@
+package com.richardpendlebury.android.moviesrev1.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ReviewResponse {
+
+    @SerializedName("id")
+    private int id;
+    @SerializedName("page")
+    private int page;
+    @SerializedName("results")
+    private List<Review> results;
+    @SerializedName("total_results")
+    private int totalResults;
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Review> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Review> results) {
+        this.results = results;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
