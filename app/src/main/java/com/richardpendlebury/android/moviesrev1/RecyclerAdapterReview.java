b0400fbf0aba86c412759166cd62c9a0ecd45d98
Richard <rick_pendlebury@hotmail.com>
2017-11-12 14:26:05 +0000

    Load models in recycler adapters

--- app/src/main/java/com/richardpendlebury/android/moviesrev1/RecyclerAdapterReview.java
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/RecyclerAdapterReview.java
@@ -1,29 +1,49 @@
 package com.richardpendlebury.android.moviesrev1;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.richardpendlebury.android.moviesrev1.model.Review;
+
+import java.util.List;
 
 public class RecyclerAdapterReview extends RecyclerView.Adapter<RecyclerAdapterReview.ViewHolder> {
 
+    private static final String LOG_TAG = "RecyclerAdapterReview";
+    private Context mContext;
+    private List<Review> mReviews;
+
+    RecyclerAdapterReview(Context context, List<Review> reviews) {
+        this.mContext = context;
+        this.mReviews = reviews;
+    }
+
     @Override
     public RecyclerAdapterReview.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.recycler_review_list_row, parent, false);
+        return new RecyclerAdapterReview.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerAdapterReview.ViewHolder holder, int position) {
-
+        final Review review = mReviews.get(position);
+        holder.tvHeader.setText(review.getAuthor());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mReviews.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvHeader;
+        ViewHolder(View itemView) {
             super(itemView);
+            tvHeader = itemView.findViewById(R.id.recycler_review_list_row_tv_header);
         }
     }
 
--- app/src/main/java/com/richardpendlebury/android/moviesrev1/RecyclerAdapterTrailer.java
+++ app/src/main/java/com/richardpendlebury/android/moviesrev1/RecyclerAdapterTrailer.java
@@ -1,29 +1,49 @@
 package com.richardpendlebury.android.moviesrev1;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.richardpendlebury.android.moviesrev1.model.Trailer;
+
+import java.util.List;
 
 public class RecyclerAdapterTrailer extends RecyclerView.Adapter<RecyclerAdapterTrailer.ViewHolder>{
 
+    private static final String LOG_TAG = "RecyclerAdapterTrailer";
+    private Context mContext;
+    private List<Trailer> mTrailers;
+
+    RecyclerAdapterTrailer(Context context, List<Trailer> trailers) {
+        this.mContext = context;
+        this.mTrailers = trailers;
+    }
+
     @Override
     public RecyclerAdapterTrailer.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-            return null;
-            }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.recycler_trailer_list_row, parent, false);
+        return new RecyclerAdapterTrailer.ViewHolder(view);
+    }
 
     @Override
     public void onBindViewHolder(RecyclerAdapterTrailer.ViewHolder holder,int position){
-
+        final Trailer trailer = mTrailers.get(position);
+        holder.tvHeader.setText(trailer.getName());
     }
 
     @Override
     public int getItemCount(){
-        return 0;
+        return mTrailers.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvHeader;
+        ViewHolder(View itemView) {
             super(itemView);
+            tvHeader = itemView.findViewById(R.id.recycler_trailer_list_row_tv_header);
         }
     }
 
